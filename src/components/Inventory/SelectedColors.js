71d9ebd522ba1588ef609a7b72f88bbed0e98588
Khallekan <okeolalekanisaac@gmail.com>
2022-04-26 14:34:04 +0100

    Change UI of selected colors and selected sizes to match add products

--- src/components/Inventory/SelectedColors.js
+++ src/components/Inventory/SelectedColors.js
@@ -1,14 +1,33 @@
 const SelectedColors = ({ colors, removeColor }) => {
   return (
-    <div className="flex flex-wrap gap-4">
-      {colors.map((item) => {
+    <div className="flex flex-wrap gap-3">
+      {colors.map((item, index) => {
         return (
           <div
-            key={item}
-            className="w-8 h-8 rounded-full cursor-pointer border-0.98 border-white-tableHeader"
-            style={{ backgroundColor: `${item}` }}
-            onClick={() => removeColor(item)}
-          ></div>
+            key={index}
+            className={`flex flex-row flex-nowrap items-center gap-1`}
+          >
+            <div
+              key={item}
+              className="w-5 h-5 rounded-full cursor-pointer border-0.98 border-white-tableHeader"
+              style={{ backgroundColor: `${item}` }}
+            ></div>
+            <svg
+              className="inline-block align-middle w-4 h-4 cursor-pointer"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              onClick={() => removeColor(item)}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </div>
         );
       })}
     </div>
--- src/components/Inventory/SelectedSizes.js
+++ src/components/Inventory/SelectedSizes.js
@@ -5,13 +5,27 @@ const SelectedSizes = ({ sizes, removeSize }) => {
     <div className="flex items-center flex-wrap gap-2">
       {sizes.map((item, index) => {
         return (
-          <div
+          <span
             key={index}
-            className="w-fit bg-white-dim rounded-md p-3 cursor-pointer"
-            onClick={() => removeSize(item)}
+            className="bg-primary-light mr-1 px-1 py-0.5 rounded-lg text-xs text-white-main flex flex-nowrap items-center"
           >
-            {item}
-          </div>
+            <span className="align-middle border-r pr-1 py-1 pl-1">{item}</span>
+            <svg
+              className="inline-block align-middle w-4 h-4 pl-1 cursor-pointer"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              onClick={() => removeSize(item)}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={3}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </span>
         );
       })}
     </div>
