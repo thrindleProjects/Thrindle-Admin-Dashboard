a243c0ddef5af65fa9c01ed0dc2bca2f6319ad99
khallekan <okeolalekanisaac@gmail.com>
2022-06-07 10:30:38 +0100

    Add drag to upload image feature to inventory modal

--- src/components/Inventory/ModalImages/BigImage/AddImageIcon/SmallAddImageIcon/index.js
+++ src/components/Inventory/ModalImages/BigImage/AddImageIcon/SmallAddImageIcon/index.js
@@ -2,14 +2,14 @@ import { IoAddSharp } from "react-icons/io5";
 
 const SmallAddImageIcon = ({ handleImageUpload }) => {
   return (
-    <div className="bg-primary-main text-white-main text-2xl h-8 w-8 rounded-md">
+    <div className="bg-primary-main text-white-main text-2xl h-8 w-8 rounded-md relative">
       <input
         type="file"
         name="image"
         multiple
         id="image"
         onChange={handleImageUpload}
-        className="hidden"
+        className="absolute w-full h-full top-0 left-0 opacity-0"
       />
       <label
         htmlFor="image"
