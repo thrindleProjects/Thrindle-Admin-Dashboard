09666669c33363df80bf5959b2e96bc9c7ac6f8c
mightyfzeus <bolarinolabisi36@gmail.com>
2022-07-28 14:15:41 +0100

    audit cleanup

--- src/pages/Audits/AdminUser.jsx
+++ src/pages/Audits/AdminUser.jsx
@@ -7,13 +7,11 @@ import ReactPaginate from "react-paginate";
 
 const AdminUser = () => {
 	const [user, setUser] = useState([]);
-	const [pageInfo, setPageInfo] = useState([]);
-
-	const [error, setError] = useState(false);
+	const [error, setError] = useState(false)
 
 	const location = useLocation();
-	console.log(pageInfo);
-	const id = location?.state?.id;
+	console.log(user);
+	const id =location?.state?.id
 
 	const getUserDetails = useCallback(async () => {
 		try {
@@ -21,47 +19,16 @@ const AdminUser = () => {
 				`/audits/${id}?service_updated=product`
 			);
 			setUser((old) => response.data.data);
-			setPageInfo((old) => response.data.pageInfo);
+			console.log(response)
 		} catch (error) {
 			console.log(error);
-			setError(true);
+			setError(true)
 		}
 	}, [id]);
 
 	useEffect(() => {
 		getUserDetails();
 	}, [getUserDetails]);
-
-	const currentPage = pageInfo?.next?.page - 1;
-	const prevPage = currentPage - 1;
-	const nextPage = pageInfo?.next?.page;
-	const totalPages = Math.ceil(pageInfo?.totalAudits / 20);
-
-	const handleNextPage = useCallback(async () => {
-		try {
-			const response = await axiosInstance.get(
-				`/audits/${id}?service_updated=product&page=${nextPage}`
-			);
-			setUser((old) => response.data.data);
-			setPageInfo((old) => response.data.pageInfo);
-		} catch (error) {
-			console.log(error);
-			setError(true);
-		}
-	}, [id, nextPage]);
-
-	const handlePrevPage = useCallback(async () => {
-		try {
-			const response = await axiosInstance.get(
-				`/audits/${id}?service_updated=product&page=${prevPage}`
-			);
-			setUser((old) => response.data.data);
-			setPageInfo((old) => response.data.pageInfo);
-		} catch (error) {
-			console.log(error);
-			setError(true);
-		}
-	}, [id, prevPage]);
 
 	// const heading = [
 	// 	"S/N",
@@ -72,75 +39,77 @@ const AdminUser = () => {
 	// 	"Category",
 	// 	"Action",
 	// ];
+	const [pageNumber, setPageNumber] = useState(0);
+
+	const usersPerPage = 10;
+
+	const pagesVisited = pageNumber * usersPerPage;
+
+	const displayProducts = user
+		.slice(pagesVisited, pagesVisited + usersPerPage)
+		.map((user, index) => {
+			return (
+				<div key={user.id}>
+					<div className="test text-gray-600 gap-10 text-sm w text-left cursor-pointer hover:bg-gray-100 hover:shadow-lg p-4 rounded-lg   px-4 ">
+						<p>{index + 1}</p>
+						<img
+							alt=""
+							className="w-20 h-10 "
+							src={`https://api.thrindle.com/api/thrindle/images/${user?.product?.images[0]}`}
+						/>
+						<p className="w-36">{user?.product?.name}</p>
+						<p>
+							N {user?.product?.original_price.toLocaleString()}
+							.00
+						</p>
+						<p> {user?.product?.market.name}</p>
+						<p> {user?.product?.category.name}</p>
+						<p> {user?.action}</p>
+						<p> {new Date(user?.product?.createdAt).toLocaleDateString()}</p>
+					</div>
+					<hr />
+				</div>
+			);
+		});
+
+	const pageCount = Math.ceil(user.length / usersPerPage);
+
+	const changePage = ({ selected }) => {
+		setPageNumber(selected);
+	};
 
 	if (error) {
-		return <p>An ERROR OCCURRED, PLEASE RELOAD THIS PAGE</p>;
+		return (
+			<p>An ERROR OCCURRED, PLEASE RELOAD THIS PAGE</p>
+		)
 	}
 
 	return (
 		<MainContainer>
-			<div className=" flex mb-10 justify-between text-lg font-bold">
+			<div className=" flex mb-10 justify-between">
 				<p>{location?.state?.name}</p>
-				{pageInfo  && (
-					<>
-						<p>Total: {pageInfo?.totalAudits}</p>
-						<p>
-							page {currentPage} of {totalPages}
-						</p>
-					</>
-				)}
+				<p>Total: {user?.length}</p>
 			</div>
-			<div className="flex justify-end mb-6 gap-4">
-				{pageInfo?.previous?.page && <p
-					className="cursor-pointer text-md font-bold"
-					onClick={handlePrevPage}
-				>
-					prev
-				</p>}
-				{pageInfo?.next?.page && <p
-					className="cursor-pointer text-md font-bold"
-					onClick={handleNextPage}
-				>
-					next
-				</p>}
+			<div className=" flex justify-end mb-10 ">
+				<div>
+					<ReactPaginate
+					breakLabel="..."
+					previousLabel={"<"}
+					nextLabel={">"}
+					pageCount={pageCount}
+					onPageChange={changePage}
+					containerClassName={"paginationBtns"}
+					previousLinkClassName={"prevBtn"}
+					nextLinkClassName={"nextBtn"}
+					disabledClassName={"paginationDisabled"}
+					activeClassName={"paginationActive"}
+					pageRangeDisplayed={2}
+					marginPagesDisplayed={2}
+				/>
+				</div>
 			</div>
-
-			<div>
-				{user.map((user, index) => (
-					<div key={user.id}>
-						<div className="test text-gray-600 gap-6 text-xs text-left cursor-pointer hover:bg-gray-100 hover:shadow-lg p-4 rounded-lg   px-4 ">
-							<p>{index + 1}</p>
-							<img
-								alt=""
-								className="w-20 h-10 "
-								src={`https://api.thrindle.com/api/thrindle/images/${user?.product?.images[0]}`}
-							/>
-							<p className="w-36">{user?.product?.name}</p>
-							<p>
-								N{" "}
-								{user?.product?.original_price.toLocaleString()}
-								.00
-							</p>
-							<p> {user?.product?.market.name}</p>
-							<p> {user?.product?.category.name}</p>
-							<p> {user?.action}</p>
-							<p>
-								
-								{new Date(
-									user?.product?.createdAt
-								).toLocaleDateString()}
-							</p>
-							<p>
-								
-								{new Date(
-									user?.product?.updatedAt
-								).toLocaleDateString()}
-							</p>
-						</div>
-						<hr />
-					</div>
-				))}
-			</div>
+			<div className="  ">{displayProducts}</div>
+			
 		</MainContainer>
 	);
 };
